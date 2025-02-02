62af0b5679560d701d513bce3ba72bb4ec6f356c
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-02 16:42:24 +0530

    Notification styling

--- components/NavBar/Notification/Notification.jsx
+++ components/NavBar/Notification/Notification.jsx
@@ -4,24 +4,24 @@ import images from "@/img"
 
 const Notification = () => {
   return (
-    <div className='notification'>
-      <p>Notification</p>
-      <div className='notification_box'>
-        <div className='notification_box_img'>
+    <div className='absolute px-8 py-4 shadow-[var(--box-shadow)] text-base w-96 rounded-2xl z-50 bg-[var(--main-bg-color)]'>
+      <p className='text-xl font-semibold mb-8'>Notification</p>
+      <div className='flex items-start gap-4 p-4 transition-all duration-300 ease-in hover:bg-[var(--icons-color)] hover:text-[var(--shadow-dark-color)] hover:rounded-3xl'> 
+        <div className='rounded-[5rem]'>
           <Image 
           src={images.user1} 
           alt="profile image"
           width={50}
           height={50}
-          className="notification_box_img"
+          className="rounded-[5rem]"
           />
         </div>
-        <div className='notification_box_info'>
-          <h4>Shoaib Akhter</h4>
-          <p>Measure action your user...</p>
+        <div>
+          <h4 className='font-semibold'>Alisha Reddy</h4>
+          <p className='text-[15px] my-2 relative '>Measure action your user...</p>
           <h6>3 minutes ago</h6>
         </div>
-        <span className='notification_box_new'></span>
+        <span className='w-2 h-2 rounded-full bg-[#00FFFF]'></span>
       </div>
     </div>
   );
