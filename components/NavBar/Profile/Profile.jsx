538c0061cd81b913ccedca53fd841f935274fc10
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-02 17:08:21 +0530

    Profile styling

--- components/NavBar/Profile/Profile.jsx
+++ components/NavBar/Profile/Profile.jsx
@@ -11,47 +11,45 @@ const Profile = () => {
     { icon: <FaUserAlt />, text: 'My Profile', link: '/myprofile' },
     { icon: <FaRegImage />, text: 'My Items', link: '/my-items' },
     { icon: <FaUserEdit />, text: 'Edit Profile', link: '/edit-profile' },
-  ];
-
-  const secondaryMenuItems = [
     { icon: <MdHelpCenter />, text: 'Help', link: '/help' },
     { icon: <TbDownload />, text: 'Disconnect', link: '/disconnect' },
   ];
 
+
   return (
-    <div className='profile'>
-      <div className='profile_account'>
-        <Image
-          src={images.user1}
-          alt='User Profile Picture'
-          width={50}
-          height={50}
-          className='profile_account_img'
-        />
-        <div className='profile_account_info'>
-          <p>Shoaib Bhai</p>
-          <h6>X08347583458...</h6>
+    <div className='absolute px-4 py-2 shadow-[var(--box-shadow)] text-4 w-80 rounded-2xl -left-36 top-16 z-50 bg-[var(--main-bg-color)]'>
+      <div className='flex-row justify-start p-2'>
+        <div className='flex items-center gap-8'>
+          <Image
+            src={images.user1}
+            alt='User Profile Picture'
+            width={50}
+            height={50}
+            className='rounded-full'
+          />
+          <div className='my-2'>
+            <p className='font-semibold'>Alisa Reddy</p>
+            <h6>X08347583458...</h6>
+          </div>
         </div>
 
-        <div className='profile_menu'>
-          <div className='profile_menu_one'>
+
             {menuItems.map((item, index) => (
-              <div key={index} className='profile_menu_one_item'>
+              <div key={index} className='flex items-center gap-6 px-2 py-4 transition-all duration-300 hover:bg-[var(--icons-color)] hover:text-[var(--shadow-dark-color)] hover:rounded-md'>
                 {item.icon}
                 <Link href={item.link}>{item.text}</Link>
               </div>
             ))}
-          </div>
-        </div>
 
-        <div className='profile_menu_two'>
+
+        {/* <div className='profile_menu_two'>
           {secondaryMenuItems.map((item, index) => (
-            <div key={index} className='profile_menu_one_item'>
+            <div key={index} className='flex items-center gap-8 px-o py-6 transition-all duration-300'>
               {item.icon}
               <Link href={item.link}>{item.text}</Link>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
