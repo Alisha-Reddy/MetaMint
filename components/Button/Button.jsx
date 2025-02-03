25b1cf5a9d47fbc618bc273eefc7c393d2210e07
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-03 20:35:10 +0530

    fixes

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -5,10 +5,9 @@ const Button = ({btnName, handleClick, styleName}) => {
     <div>
       <button className={
         `${ styleName ? styleName : 
-          'bg-[var(--ele-bg-color)] px-3 py-1 rounded-3xl text-lg text-[var(--ele-hover-bg-color)] cursor-pointer transition-all duration-200 ease-in hover:shadow-xl hover:shadow-[var(--shadow-light-color)] hover:bg-[--ele-hover-bg-color] hover:text-[var(--ele-bg-color)]'}`
+          'bg-[var(--ele-bg-color)] px-3 py-1 rounded-3xl text-lg text-[var(--icons-color)] cursor-pointer transition-all duration-200 ease-in hover:shadow-[.5rem_.5rem_1rem] hover:shadow-[var(--shadow-light-color)] hover:bg-[--ele-hover-bg-color] hover:text-[var(--icons-dark-color)]'}`
         } 
       onClick={()=> handleClick}> 
-      {/* <button className='glowing' onClick={()=> handleClick}> */}
         {btnName}
       </button>
     </div>
