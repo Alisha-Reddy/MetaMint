2a1a5aa4c241b2d54a5a5cde73b8a3ed71f6dfc7
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-05 16:09:11 +0530

    Foter styling

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,9 +14,9 @@ import { Discover, HelpCenter } from '../NavBar'
 
 const Footer = () => {
   return (
-    <div className='w-full relative p-6'>{/* footer */}
-      <div className='w-[95%] mx-0 my-auto grid gris-cols[1fr]  sm:grid-cols-[1fr_1fr_1fr_2fr] items-start justify-between gap-12'> {/* footer_box */}
-        <div className='flex flex-col items-center justify-center gap-2'> {/* footer_box_social */}
+    <div className='w-full relative p-6'>
+      <div className='w-[95%] mx-0 my-auto grid gris-cols[1fr]  sm:grid-cols-[1fr_1fr_1fr_2fr] items-start justify-between gap-12'>
+        <div className='flex flex-col items-center justify-center gap-4'>
           <Image
           src={images.logo}
           alt='Logo'
@@ -24,7 +24,7 @@ const Footer = () => {
           width={100}
           />
           <p> Empowering creators and collectors with a seamless, cutting-edge NFT experience.</p>
-            <div className='flex gap-5 text-xl items-center'>{/* footer_social */}
+            <div className='flex gap-5 text-xl items-center'>
               <a href="#" className='p-1 rounded-full transition-all duration-300 ease-in grid hover:bg-[var(--icons-color)] hover:text-[var(--modal-solid-bg)] hover:shadow-md hover:shadow-[var(--shadow-dark-color)]'>
                 <TiSocialFacebook/>
               </a>
@@ -43,24 +43,20 @@ const Footer = () => {
             </div>
         </div>
         <div>
-          {/* footer_box_discover */}
           <h1 className='font-bold text-xl'>Discover</h1>
           <Discover/>
         </div>
         <div>
-          {/* footer_box_Help */}
           <h1 className='font-bold text-xl'>Help Center</h1>
           <HelpCenter/>
         </div>
         <div>
-          {/* subscribe */}
           <h1 className='font-bold text-xl'>Subscribe</h1>
           <div className='w-full flex justify-between items-center px-6 py-3 bg-[var(--icons-bg-color)] rounded-3xl mt-4'>
-            {/* subscribe_box */}
             <input type="email" placeholder='Enter your email' className='bg-transparent border-none outline-none w-[90%] text-[var(--icons-dark-color)] placeholder-[var(--icons-dark-color)]' />
-            <RiSendPlaneFill className='text-[var(--icons-dark-color)] text-2xl cursor-pointer' /> {/* subscribe_box_send */}
+            <RiSendPlaneFill className='text-[var(--icons-dark-color)] text-2xl cursor-pointer' /> 
           </div>
-          <div className='px-2 py-8'> {/* subscribe_box_info */}
+          <div className='px-2 py-8'>
             <p>Discover, mint, and trade NFTs seamlessly with our next-gen decentralized app. </p>
           </div>
         </div>
