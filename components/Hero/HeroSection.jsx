361dfe95c7c6428508493fea0dd31e34cc9b2a5b
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-05 17:36:12 +0530

    Hero img

--- components/Hero/HeroSection.jsx
+++ components/Hero/HeroSection.jsx
@@ -6,9 +6,9 @@ import {Button} from "../index"
 
 const HeroSection = () => {
   return (
-    <div className='w-[95%] sm:w-[90%] mx-12 my-4 sm:my-10'>{/* hroSection */}
-        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8'>{/* hroSection_box */}
-            <div>{/* hroSection_box_left */}
+    <div className='w-[95%] sm:w-[90%] mx-12 my-4 sm:my-10'>
+        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8'>
+            <div className='pr-8 sm:pr-24'>
                 <Image 
                 src={images.hero} 
                 alt='Hero section' 
@@ -16,7 +16,7 @@ const HeroSection = () => {
                 height={600}
                 />
             </div>
-            <div className='pr-8 sm:pr-24'>{/* hroSection_box_left */}
+            <div className='pr-8 sm:pr-24'>
                 <h1 className='text-3xl sm:text-5xl py-1 sm:py-4'>Discover, mint, and trade NFTs seamlessly with our next-gen dapp.</h1>
                 <p className='mb-8 '>Empowering creators and collectors with a seamless, cutting-edge NFT experience.</p>
                 <Button btnName="Start your search" styleName='glowing' handleClick={()=>{}}/>
