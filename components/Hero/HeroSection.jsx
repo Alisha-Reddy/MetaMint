2c9aea4629bf171fb62076cb08ea1d06d12a3c20
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-06 15:49:20 +0530

    Service styling

--- components/Hero/HeroSection.jsx
+++ components/Hero/HeroSection.jsx
@@ -6,16 +6,15 @@ import {Button} from "../index"
 
 const HeroSection = () => {
   return (
-    <div className='w-[95%] sm:w-[90%] mx-7 my-4 sm:my-10'>
-        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8 items-center'>
-            <div className='pr-8 sm:pr-24'>
+    <div className='w-[90%] mx-7 my-4 sm:my-10'>
+        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8 justify-items-center justify-center items-center text-center'>
+            
                 <Image 
                 src={images.hero} 
                 alt='Hero section' 
-                width={600}
-                height={600}
+                className='w-[100%] lg:w-[90%]'
                 />
-            </div>
+            
             <div className='pr-8 sm:pr-24'>
                 <h1 className='text-xl sm:text-3xl md:text-5xl py-1 sm:py-4'>Discover, mint, and trade NFTs seamlessly with our next-gen dapp.</h1>
                 <p className='mb-8 text-xs sm:text-sm md:text-base'>Empowering creators and collectors with a seamless, cutting-edge NFT experience.</p>
--- components/ServiceSection/ServiceSection.jsx
+++ components/ServiceSection/ServiceSection.jsx
@@ -5,66 +5,58 @@ import images from "@/img"
 const ServiceSection = () => {
   return (
     <div className='w-[95%] sm:w-[90%] mx-12 my-auto '>{/* service */}
-      <div className='grid grid-cols-4 gap-4 sm:gap-12 items-center text-center'>{/* service_box */}
-        <div>{/* service_box_item */}
-          <div>{/* service1 */}
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-12 text-center justify-items-center'>{/* service_box */}
+          <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
               src={images.service1}
               alt = "Filter & Disover"
               width={100}
               height={100}
             />
-            <p className=''>{/* sevice_box_item_step */}
-              <span>Step 1</span>
+            <p className='my-4'>{/* sevice_box_item_step */}
+              <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 1</span>
             </p>
-            <h3>Filter & Discover</h3>
+            <h3 className='font-semibold'>Filter & Discover</h3>
             <p>description 1</p>
-          </div>
         </div>
-        <div>{/* service_box_item */}
-          <div>{/* service1 */}
-            <Image 
-              src={images.service2}
-              alt = "Connect Wallet"
-              width={100}
-              height={100}
-            />
-            <p className=''>{/* sevice_box_item_step */}
-              <span>Step 2</span>
-            </p>
-            <h3>Connect Wallet</h3>
-            <p>description 2</p>
-          </div>
-        </div>
-        <div>{/* service_box_item */}
-          <div>{/* service1 */}
-            <Image 
-              src={images.service3}
-              alt = "Start Trading"
-              width={100}
-              height={100}
-            />
-            <p className=''>{/* sevice_box_item_step */}
-              <span>Step 3</span>
-            </p>
-            <h3>Start Trading</h3>
-            <p>description 3</p>
-          </div>
-        </div>
-        <div>{/* service_box_item */}
-          <div>{/* service1 */}
+        <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
               src={images.service4}
               alt = "Filter & Disover"
               width={100}
               height={100}
             />
-            <p className=''>{/* sevice_box_item_step */}
-              <span>Step 4</span>
+            <p className='my-4'>{/* sevice_box_item_step */}
+            <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 4</span>
             </p>
-            <h3>Filter & Discover</h3>
+            <h3 className='font-semibold'>Filter & Discover</h3>
             <p>description 4</p>
-          </div>
+        </div>
+        <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
+            <Image 
+              src={images.service2}
+              alt = "Connect Wallet"
+              width={100}
+              height={100}
+            />
+            <p className='my-4'>{/* sevice_box_item_step */}
+            <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 2</span>
+            </p>
+            <h3 className='font-semibold'>Connect Wallet</h3>
+            <p>description 2</p>
+        </div>
+        <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
+            <Image 
+              src={images.service3}
+              alt = "Start Trading"
+              width={100}
+              height={100}
+            />
+            <p className='my-4'>{/* sevice_box_item_step */}
+            <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 3</span>
+            </p>
+            <h3 className='font-semibold'>Start Trading</h3>
+            <p>description 3</p>
         </div>
       </div>
     </div>
