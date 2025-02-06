fcdc8673acb21db93f0237f554950945edf1ec73
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-06 17:17:25 +0530

    service imgs

--- components/ServiceSection/ServiceSection.jsx
+++ components/ServiceSection/ServiceSection.jsx
@@ -9,54 +9,54 @@ const ServiceSection = () => {
           <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
               src={images.service1}
-              alt = "Filter & Disover"
+              alt = "Browse & Discover "
               width={100}
               height={100}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
               <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 1</span>
             </p>
-            <h3 className='font-semibold'>Filter & Discover</h3>
-            <p>description 1</p>
+            <h3 className='font-semibold'>Browse & Discover</h3>
+            <p>Find unique NFTs from top creators. No stress, just scroll.1</p>
         </div>
         <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
               src={images.service4}
-              alt = "Filter & Disover"
-              width={100}
-              height={100}
-            />
-            <p className='my-4'>{/* sevice_box_item_step */}
-            <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 4</span>
-            </p>
-            <h3 className='font-semibold'>Filter & Discover</h3>
-            <p>description 4</p>
-        </div>
-        <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
-            <Image 
-              src={images.service2}
-              alt = "Connect Wallet"
+              alt = "Mint & Create"
               width={100}
               height={100}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
             <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 2</span>
             </p>
-            <h3 className='font-semibold'>Connect Wallet</h3>
-            <p>description 2</p>
+            <h3 className='font-semibold'>Mint & Create</h3>
+            <p>Turn your art into NFTs in a few clicks. No coding needed.</p>
         </div>
         <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
-              src={images.service3}
-              alt = "Start Trading"
+              src={images.service2}
+              alt = "Buy & Sell "
               width={100}
               height={100}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
             <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 3</span>
             </p>
-            <h3 className='font-semibold'>Start Trading</h3>
-            <p>description 3</p>
+            <h3 className='font-semibold'>Buy & Sell </h3>
+            <p>Trade NFTs easily. No complicated steps.</p>
+        </div>
+        <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
+            <Image 
+              src={images.service3}
+              alt = "Track & Collect"
+              width={100}
+              height={100}
+            />
+            <p className='my-4'>{/* sevice_box_item_step */}
+            <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 4</span>
+            </p>
+            <h3 className='font-semibold'>Track & Collect</h3>
+            <p>Build your collection, track value, and flex.</p>
         </div>
       </div>
     </div>
