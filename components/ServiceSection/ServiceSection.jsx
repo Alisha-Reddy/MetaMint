e28c7db44738d5feaff06054f42def0040365655
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-07 11:20:50 +0530

    Service styling

--- components/ServiceSection/ServiceSection.jsx
+++ components/ServiceSection/ServiceSection.jsx
@@ -12,51 +12,59 @@ const ServiceSection = () => {
               alt = "Browse & Discover "
               width={100}
               height={100}
+              style={{ filter: 'drop-shadow(4px 4px 5px #0000ff98)' }}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
               <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 1</span>
             </p>
             <h3 className='font-semibold'>Browse & Discover</h3>
-            <p>Find unique NFTs from top creators. No stress, just scroll.1</p>
+            <p>Find unique NFTs from top creators. <br />
+               No stress, just scroll.</p>
         </div>
         <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
-              src={images.service4}
+              src={images.service2}
               alt = "Mint & Create"
               width={100}
               height={100}
+              style={{ filter: 'drop-shadow(4px 4px 5px #0000ff98)' }}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
             <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 2</span>
             </p>
             <h3 className='font-semibold'>Mint & Create</h3>
-            <p>Turn your art into NFTs in a few clicks. No coding needed.</p>
+            <p>Turn your art into NFTs in a few clicks. <br />
+               No coding needed.</p>
         </div>
         <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
-              src={images.service2}
+              src={images.service3}
               alt = "Buy & Sell "
               width={100}
               height={100}
+              style={{ filter: 'drop-shadow(4px 4px 5px #0000ff98)' }}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
             <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 3</span>
             </p>
             <h3 className='font-semibold'>Buy & Sell </h3>
-            <p>Trade NFTs easily. No complicated steps.</p>
+            <p>Trade NFTs easily. <br />
+               No complicated steps.</p>
         </div>
         <div className='flex flex-col items-center sm:gap-2'>{/* service1 */}
             <Image 
-              src={images.service3}
+              src={images.service4}
               alt = "Track & Collect"
               width={100}
               height={100}
+              style={{ filter: 'drop-shadow(4px 4px 5px #0000ff98)' }}
             />
             <p className='my-4'>{/* sevice_box_item_step */}
             <span className='px-6 py-2 bg-[var(--shadow-dark-color)] text-[var(--icons-dark-color)] rounded-2xl'>Step 4</span>
             </p>
             <h3 className='font-semibold'>Track & Collect</h3>
-            <p>Build your collection, track value, and flex.</p>
+            <p>Build your collection, track value, and flex. <br />
+            No FOMO, just vibes. </p>
         </div>
       </div>
     </div>
