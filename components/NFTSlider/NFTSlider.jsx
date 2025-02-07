2d65f589e72ed84869c5a4f901c4f82b6a1f0a5c
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-07 11:26:24 +0530

    NFT Slider set up

---
+++ components/NFTSlider/NFTSlider.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React, {useState, useEffect} from 'react'
+import Image from 'next/image'
+import {AiFillFire} from 'react-icons/ai'
+import {MdVerified} from 'react-icons/md'
+
+
+const NFTSlider = () => {
+  return (
+    <div>NFTSlider</div>
+  )
+}
+
+export default NFTSlider
