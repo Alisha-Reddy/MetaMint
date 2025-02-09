72886a9f453380205ed6dd15f5c8534a4cde7c0e
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-09 15:08:18 +0530

    NFT slider adjusting to mobile

--- components/NFTSlider/NFTSlider.jsx
+++ components/NFTSlider/NFTSlider.jsx
@@ -95,12 +95,12 @@ const NFTSlider = () => {
     // },[])
 
   return (
-        <div className=' w-[95%] mx-auto pt-60 sm:py-28 sm:px-12 grid md:grid-cols-[repeat(12,1fr)] items-center gap-4 sm:gap-0'>{/* bigNFTSlider_box */}
-            <div className='order-1 md:order-2 col-start-4 col-end-8 row-start-1 row-end-[- 1] bg-[var(--main-bg-color)] shadow-[5px_5px_20px] shadow-[var(--shadow-light-color)] rounded-2xl p-8 z-10 h-[68vh]'>{/* bigNFTSlider_box_left */}
-                <h2 className='text-5xl font-bold'>{sliderData[idNumber].title}</h2>
+        <div className=' w-[95%] mx-auto p-4  sm:py-28 sm:px-12 md:grid md:grid-cols-[repeat(12,1fr)] items-center sm:gap-0'>{/* bigNFTSlider_box */}
+            <div className=' md:col-start-4 md:col-end-8 md:row-start-1 md:row-end-[- 1] bg-[var(--main-bg-color)] shadow-[5px_5px_20px] shadow-[var(--shadow-light-color)] rounded-2xl p-4 md:p-8 z-10 h-[60vh] md:h-[68vh]'>{/* bigNFTSlider_box_left */}
+                <h2 className='text-2xl md:text-5xl font-bold'>{sliderData[idNumber].title}</h2>
 
                 <div className='flex items-center'>{/* bigNFTSlider_box_left_creator */}
-                    <div className='flex flex-1 items-center gap-4'>{/* bigNFTSlider_box_left_creator_profile */}
+                    <div className='flex flex-1 items-center gap-1 md:gap-4 text-xs md:text-base'>{/* bigNFTSlider_box_left_creator_profile */}
                         <Image 
                         src={sliderData[idNumber].image}
                         alt = "Profile image"
@@ -117,8 +117,8 @@ const NFTSlider = () => {
                             </h4>
                         </div>
                     </div>
-                    <div className='flex flex-1 items-center gap-4'>{/* bigNFTSlider_box_left_creator_collection */}
-                        <AiFillFire className='text-5xl' /> {/* bigNFTSlider_box_left_creator_collection_icon */}
+                    <div className='flex flex-1 items-center gap-1 md:gap-4 text-xs md:text-base'>{/* bigNFTSlider_box_left_creator_collection */}
+                        <AiFillFire className='text-2xl md:text-5xl' /> {/* bigNFTSlider_box_left_creator_collection_icon */}
                         <div>{/* bigNFTSlider_box_left_creator_collection_info */}
                             <p className='mt-2'>Collection</p>
                             <h4 className='font-semibold'>{sliderData[idNumber].collection}</h4>
@@ -126,33 +126,33 @@ const NFTSlider = () => {
                     </div>
                 </div>
                 <div>{/* bigNFTSlider_box_left_bidding */}
-                    <div className='border-2 border-[var(--icons-dark-color)] my-4 px-10 py-4 rounded-lg relative'>{/* bigNFTSlider_box_left_bidding_box */}
-                        <small className='absolute -top-5 bg-[var(--main-bg-color)] p-3 rounded-lg font-semibold'>Current Bid</small>
+                    <div className='border-2 border-[var(--icons-dark-color)] my-4 px-4 md:px-10 pb-2 md:py-4 rounded-lg relative text-xs md:text-base'>{/* bigNFTSlider_box_left_bidding_box */}
+                        <small className='absolute -top-3 md:-top-5 bg-[var(--main-bg-color)] p-1 md:p-3 rounded-lg font-semibold'>Current Bid</small>
                         <p className='inline-flex gap-1 mt-4 '>{sliderData[idNumber].price} - 
                             <span>$221,21</span>
                         </p>
                     </div>
-                    <p className='flex items-center justify-center gap-4 mt-2'>{/* bigNFTSlider_box_left_bidding_box_auction */}
+                    <p className='flex items-center justify-center gap-4 mt-2 text-sm md:text-base'>{/* bigNFTSlider_box_left_bidding_box_auction */}
                         <MdTimer className='text-3xl' />
                         {/* bigNFTSlider_box_left_bidding_box_icon */}
                         <span >Auction ending in</span>
                     </p>
-                    <div className='mx-auto flex justify-between text-center p-4'>
+                    <div className='mx-auto flex justify-between text-center md:p-4'>
                         {/* bigNFTSlider_box_left_bidding_box_timer*/}
                         <div>{/* bigNFTSlider_box_left_bidding_box_timer_item*/}
-                            <p className='text-3xl font-bold '> {sliderData[idNumber].time.day} </p>
+                            <p className='text-xl md:text-3xl font-bold '> {sliderData[idNumber].time.day} </p>
                             <span>Days</span>
                         </div>
                         <div>{/* bigNFTSlider_box_left_bidding_box_timer_item*/}
-                            <p className='text-3xl font-bold '> {sliderData[idNumber].time.hours} </p>
+                            <p className='text-xl md:text-3xl font-bold '> {sliderData[idNumber].time.hours} </p>
                             <span>Hours</span>
                         </div>
                         <div>{/* bigNFTSlider_box_left_bidding_box_timer_item*/}
-                            <p className='text-3xl font-bold '> {sliderData[idNumber].time.minutes} </p>
+                            <p className='text-xl md:text-3xl font-bold '> {sliderData[idNumber].time.minutes} </p>
                             <span>Minutes</span>
                         </div>
                         <div>{/* bigNFTSlider_box_left_bidding_box_timer_item*/}
-                            <p className='text-3xl font-bold '> {sliderData[idNumber].time.seconds} </p>
+                            <p className='text-xl md:text-3xl font-bold '> {sliderData[idNumber].time.seconds} </p>
                             <span>Seconds</span>
                         </div>
                     </div>
@@ -174,14 +174,14 @@ const NFTSlider = () => {
                 </div>
 
             </div>
-            <div className='order-2 md:order-1 col-start-7 col-end-12 row-start-1 row-end-[-1] shadow-[10px_10px_20px] shadow-[var(--shadow-light-color)] rounded-2xl p-4 h-[85vh]'>{/* bigNFTSlider_box_right */}
+            <div className='mt-10 sm:mt-0 md:col-start-7 md:col-end-12 md:row-start-1 md:row-end-[-1] md:shadow-[10px_10px_20px] shadow-[var(--shadow-light-color)] rounded-2xl md:p-4 md:h-[85vh]'>{/* bigNFTSlider_box_right */}
                 <div className='relative w-full h-full'>{/* bigNFTSlider_box_right_box */}
                     <Image
                     src={sliderData[idNumber].nftImage}
                     alt='NFT IMAGE'
                     className="w-full h-full object-fill rounded-3xl"
                     />
-                    <div className='absolute top-8 right-8 flex items-center gap-2 text-2xl bg-red-100 text-red-500 p-2 rounded-2xl'>{/* bigNFTSlider_box_right_box_like */}
+                    <div className='absolute -top-2 md:top-8 -right-2 md:right-8 flex items-center md:gap-2 text-sm md:text-2xl bg-red-100 text-red-500 p-1 md:p-2 rounded-sm md:rounded-2xl'>{/* bigNFTSlider_box_right_box_like */}
                         <AiFillHeart className='text-red-500' />
                         <span>{sliderData[idNumber].like}</span>
                     </div>
