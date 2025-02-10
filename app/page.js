38471f5f5154fa73af7c7379209d846ec1b1309a
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-10 15:09:52 +0530

    Title set up

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import {HeroSection, ServiceSection, NFTSlider, Subscribe} from "../components"
+import {HeroSection, ServiceSection, NFTSlider, Subscribe, Title} from "../components"
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <ServiceSection/>
       <NFTSlider />
       <Subscribe />
+      <Title />
     </main>
   );
 }
---
+++ components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = ({heading, paragraph}) => {
+  return (
+    <div>Title</div>
+  )
+}
+
+export default Title
