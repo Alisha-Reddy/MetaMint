c581bbbc3448c1f42712fd4bf20e4af0fc4fad53
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-10 14:54:46 +0530

    Subscribe styling

--- components/Subscribe/Subscribe.jsx
+++ components/Subscribe/Subscribe.jsx
@@ -5,25 +5,25 @@ import images from "@/img"
 
 const Subscribe = () => {
   return (
-    <div>{/* subscribe */}
-        <div>{/* subscribe_box */}
+    <div className='w-[95%] md:w-[90%] mb-8'>{/* subscribe */}
+        <div className='w-[85%] mx-auto grid grids-cols-2 md:grid-cols-[1.5fr_2fr] items-center'>{/* subscribe_box */}
             <div>{/* subscribe_box_left */}
-                <h2>Never miss a drop</h2>
-                <p>Big things are coming! Subscribe to get early access, updates, and exclusive drops.  No spam, just vibes.</p>
+                <h2 className='text-3xl mb-2 font-semibold'>Never miss a drop</h2>
+                <p className='text-lg'>Big things are coming! Subscribe to get early access, updates, and exclusive drops.  No spam, just vibes.</p>
                 
-                <div>{/* subscribe_box_left_box */}
-                    <span>01</span>
-                    <small>Get more discount</small>
+                <div className='flex items-center gap-1 mt-8'>{/* subscribe_box_left_box */}
+                    <span className='bg-[var(--icons-bg-color)] text-[var(--main-bg-color)] px-2 py-1 rounded-xl  text-center mr-1'>01</span>
+                    <small className='font-bold text-lg'>Get more discount</small>
                 </div>
 
-                <div>{/* subscribe_box_left_box */}
-                    <span>02</span>
-                    <small>Get premium magazines</small>
+                <div className='flex items-center gap-1 mt-8'>{/* subscribe_box_left_box */}
+                    <span className='bg-[var(--icons-bg-color)] text-[var(--main-bg-color)] px-2 py-1 rounded-xl  text-center mr-1'>02</span>
+                    <small className='font-bold text-lg'>Get premium magazines</small>
                 </div>
 
-                <div>{/* subscribe_box_left_input */}
-                    <input type="email" placeholder='Enter your email' />
-                    <RiSendPlaneFill/>{/* subscribe_box_left_input_icon */}
+                <div className='mt-12 p-4 w-[80%] rounded-xl bg-[var(--icons-bg-color)] text-[var(--main-bg-color)] flex items-center'>{/* subscribe_box_left_input */}
+                    <input type="email" placeholder='Enter your email' className='bg-transparent border-none outline-none w-[90%] placeholder:text-[var(--main-bg-color)]'/>
+                    <RiSendPlaneFill className='text-3xl'/>{/* subscribe_box_left_input_icon */}
                 </div>
             </div>
 
