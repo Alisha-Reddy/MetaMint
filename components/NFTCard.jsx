e4f9f71b6ffdfb74746201d3e4b89fd4aca9a2ff
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-13 12:02:56 +0530

    NFT Card

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import Image from "next/image";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { BsImages } from "react-icons/bs";
+import images from "@/img";
 
 const NFTCard = () => {
-  return (
-    <div>NFTCard</div>
-  )
-}
+  const featureArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [like, setLike] = useState(true);
+  const Style = {
+    
+  }
 
-export default NFTCard
+  return (
+    <div className={Style.NFTCard}>
+      {featureArray.map(el => (
+        <div className={Style.NFTCard_box} key={el}>
+          <div className={Style.NFTCard_box_img}>
+            <Image
+              src={images.nftImage1}
+              alt="NFT image"
+              width={600}
+              height={600}
+              className={Style.NFTCard_box_img_img}
+            />
+          </div>
+
+          <div className={Style.NFTCard_box_update}>
+            <div className={Style.NFTCard_box_update_left}>
+              <div
+                className={Style.NFTCard_box_update_left_like}
+                onClick={() => {setLike(prev => !prev)}}
+              >
+                {like ? <AiOutlineHeart /> : <AiFillHeart className={Style.NFTCard_box_update_left_like_icon} />}
+                22
+              </div>
+            </div>
+
+            <div className={Style.NFTCard_box_update_right}>
+              <div className={Style.NFTCard_box_update_right_info}>
+                <small>Remaining time</small>
+                <p>3h : 15m : 20s</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={Style.NFTCard_box_update_details}>
+            <div className={Style.NFTCard_box_update_details_price}>
+              <div className={Style.NFTCard_box_update_details_price_box}>
+                <h4>Clone #17373</h4>
+
+                <div className={Style.NFTCard_box_update_details_price_box_box}>
+                  <div className={Style.NFTCard_box_update_details_price_box_bid}>
+                    <small>Current Bid</small>
+                    <p>1.000ETH</p>
+                  </div>
+                  <div className={Style.NFTCard_box_update_details_price_box_stock}>
+                    <small>61 in stock</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={Style.NFTCard_box_update_details_category}>
+              <BsImages />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NFTCard;
