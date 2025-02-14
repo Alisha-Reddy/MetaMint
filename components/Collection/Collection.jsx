f63d63a7e94ec31092bcbdca22bcf44e231e7713
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-14 15:34:23 +0530

    Collection Styling

--- components/Collection/Collection.jsx
+++ components/Collection/Collection.jsx
@@ -1,6 +1,10 @@
 "use client";
 import React, { useState } from "react";
-import { BsFillAlarmFill, BsFillCalendarDateFill, BsCalendar3 } from "react-icons/bs";
+import {
+  BsFillAlarmFill,
+  BsFillCalendarDateFill,
+  BsCalendar3,
+} from "react-icons/bs";
 import { Title } from "..";
 import DaysComponent from "./DaysComponent";
 
@@ -10,7 +14,7 @@ const Collection = () => {
   const data = {
     popular: [1, 2, 3, 4, 5, 6, 7, 8],
     following: [1, 2, 3, 4],
-    news: [1, 2, 3, 4, 5, 6]
+    news: [1, 2, 3, 4, 5, 6],
   };
 
   const handleTabChange = (tab) => {
@@ -19,35 +23,40 @@ const Collection = () => {
 
   return (
     <div className="w-[95%] md:w-[90%] mx-auto">
-      <Title 
-        heading="Flex Game Strong!"
-        paragraph="Peek at the champs. Feel the FOMO."
-      />
-
+      <Title heading="Flex Game Strong!" paragraph="Peek at the champs. Feel the FOMO." />
 
       <div className="w-[100%] py-3 pb-10">
-        <div className="w-96 mx-auto py-6 text-center">
-          <h2 className="text-5xl mb-4">Top List Creators</h2>
-          <div className="collection_collections">
-            <div className="bg-[var(--main-bg-color)] p-1 rounded-full flex justify-around items-center gap-4 text-lg shadow- ">
+        <div className=" w-full md:w-[430px] pb-10 mx-auto  text-center items-center">
+          <h2 className="text-xl md:text-5xl mb-4">Top List Creators</h2>
+          <div>
+            <div className="bg-[var(--main-bg-color)] py-4 px-1 md:px-2 rounded-full flex justify-around items-center gap-1 md:gap-4 text-sm md:text-lg shadow-md">
               <button
                 onClick={() => handleTabChange("popular")}
-                // className={activeTab === "popular" ? "active" : ""}
-                className="bg-[var(--icons-color)] text-[var(--main-bg-color)] p-4 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)]"
+                className={`p-2 flex justify-center items-center gap-1 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)] ${
+                  activeTab === "popular"
+                    ? "bg-[var(--icons-color)] text-[var(--main-bg-color)] shadow-[0_0_10px_var(--icons-color)] scale-105"
+                    : "bg-transparent text-[var(--icons-color)]"
+                }`}
               >
                 <BsFillAlarmFill /> 24 hours
               </button>
               <button
                 onClick={() => handleTabChange("following")}
-                // className={activeTab === "following" ? "active" : ""}
-                className="bg-[var(--icons-color)] text-[var(--main-bg-color)] p-4 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)]"
+                className={`p-2 flex justify-center items-center gap-1 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)] ${
+                  activeTab === "following"
+                    ? "bg-[var(--icons-color)] text-[var(--main-bg-color)] shadow-[0_0_10px_var(--icons-color)] scale-105"
+                    : "bg-transparent text-[var(--icons-color)]"
+                }`}
               >
                 <BsCalendar3 /> 7 days
               </button>
               <button
                 onClick={() => handleTabChange("news")}
-                // className={activeTab === "news" ? "active" : ""}
-                className="bg-[var(--icons-color)] text-[var(--main-bg-color)] p-4 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)]"
+                className={`p-2 flex justify-center items-center gap-1 rounded-full cursor-pointer border border-[var(--icons-color)] transition-all duration-300 ease-in hover:bg-transparent hover:text-[var(--icons-color)] ${
+                  activeTab === "news"
+                    ? "bg-[var(--icons-color)] text-[var(--main-bg-color)] shadow-[0_0_10px_var(--icons-color)] scale-105"
+                    : "bg-transparent text-[var(--icons-color)]"
+                }`}
               >
                 <BsFillCalendarDateFill /> 30 days
               </button>
@@ -55,9 +64,9 @@ const Collection = () => {
           </div>
         </div>
 
-        <div className="w-[80%] mx-auto grid grid-cols-3 gap-8">
-          {data[activeTab].map((el, i) => (
-            <DaysComponent key={i} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {data[activeTab].map((el) => (
+            <DaysComponent key={el} />
           ))}
         </div>
       </div>
--- components/Collection/DaysComponent.jsx
+++ components/Collection/DaysComponent.jsx
@@ -5,46 +5,39 @@ import images from "@/img";
 
 const DaysComponent = () => {
   return (
-    <div className="w-[100%] rounded-2xl transition-all duration-300 ease-in cursor-pointer hover:shadow-[0px_px_10px_5px_var(--box-shadow)]">
-      <div className="daysComponent_box">
+    <div className="w-[100%] rounded-2xl transition-all duration-300 ease-in cursor-pointer hover:shadow-[8px_8px_20px_5px_var(--shadow-light-color)] bg-[var(--bg-color)] p-2 hover:-translate-y-2 hover:rotate-1 hover:scale-105">
         <div className="rounded-2xl">
           <Image
             src={images.creatorBackground1}
             alt="profile background"
-            width={500}
-            height={300}
-            // className="w-full h-auto object-cover rounded-lg"
+            className="w-full h-auto object-cover rounded-lg"
           />
         </div>
 
-        <div className="grid grid-cols-3 gap-1">
+        <div className="grid grid-cols-3 gap-1 mt-2 rounded-b-2xl overflow-hidden">
           {[...Array(3)].map((_, i) => (
             <Image
               key={i}
               src={images.creatorBackground2}
               alt={`profile ${i + 1}`}
-              width={200}
-              height={200}
-              className="rounded-2xl"
+              className="w-full h-auto object-cover"
             />
           ))}
         </div>
 
         <div className="p-2">
           <h2 className="text-xl font-semibold">Amazing Collection</h2>
-          <div className="flex gap-1 items-center mt-2">
-            <div className="flex items-center justify-between">
+          <div className="flex gap-1 justify-between items-center mt-2">
+            <div className="flex  gap-4 justify-center items-center">
               <Image
                 src={images.user1}
                 alt="profile"
-                width={30}
-                height={30}
-                className="rounded-full"
+                className="w-11 rounded-full"
               />
-              <p className="text-sm text-gray-700">
+              <p className="text-sm ">
                 Creator
                 <span className="flex items-center space-x-1">
-                  <span className="font-semibold">Shoaib Bhai</span>
+                  <span className="font-semibold">Alena</span>
                   <MdVerified className="text-blue-500" />
                 </span>
               </p>
@@ -54,7 +47,7 @@ const DaysComponent = () => {
             </div>
           </div>
         </div>
-      </div>
+      
     </div>
   );
 };
