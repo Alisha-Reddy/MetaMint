00f71b625ae06c5bda72c08ef6215d0d6edeed91
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-25 17:18:05 +0530

    collection page styling

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -7,13 +7,13 @@ import { Button } from '.'
 const Brand = () => {
   return (
     <div>
-      <div className="my-4 sm:my-14 grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-[5rem] items-center">
+      <div className="my-4 sm:my-14 grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-20 items-center">
         <div>
           <Image src={images.logo} alt="brand logo" width={100} height={100} />
-          <h1 className="text-[5rem] leading-[1.3] font-bold md:text-[2.5rem]">Collect, Trade & Earn Crypto with MetaMint!</h1>
-          <p className="text-[1.3rem] mt-[-2rem] md:mt-[-0.5rem]">ump into the world of NFTs! Buy, sell, and discover rare digital treasures.</p>
+          <h1 className="text-2xl my-3 font-bold md:text-5xl">Collect, Trade & Earn Crypto with MetaMint!</h1>
+          <p className="md:text-base text-sm">Jump into the world of NFTs! Buy, sell, and discover rare digital treasures.</p>
 
-          <div className='flex justify-between items-center mt-4 sm:mx-4'>
+          <div className='flex justify-between items-center mt-4 sm:pr-10 sm:mx-4'>
             <Button btnName="Create"  handleClick={()=>{}}/>
             <Button btnName="Discover"  handleClick={()=>{}}/>
           </div>
