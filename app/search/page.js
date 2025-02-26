73379e24cd4e886387ec70c11d9bbdd48c8872a9
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-26 16:57:59 +0530

    search page

--- app/search/page.js
+++ app/search/page.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Slider, Brand, Filter } from '@/components'
+import { SearchBar } from '@/components/SearchPage'
+import { NFTCard, Banner } from '@/components/CollectionPage'
+import images from "@/img"
 
 const page = () => {
+  const collectionArray = [
+      images.nftImage1,
+      images.nftImage2,
+      images.nftImage3,
+      images.nftImage1,
+      images.nftImage2,
+      images.nftImage3,
+      images.nftImage1,
+      images.nftImage2,
+      images.nftImage3,
+    ];
   return (
-    <div>page</div>
+    <div>
+      <Banner bannerImage={images.creatorBackground3} />
+      <SearchBar/>
+      <Filter/>
+      <NFTCard NFTData={collectionArray}/>
+      <Slider/>
+      <Brand/>
+    </div>
   )
 }
 
