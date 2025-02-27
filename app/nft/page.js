c7e6c7ffb06d1cddae9bc8e2a727947f16f1fef5
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-02-27 15:55:20 +0530

    NFT Details

--- app/nft/page.js
+++ app/nft/page.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button, Brand } from '@/components'
+import { Category } from '@/components/MainPage'
+import { Details } from '@/components/NFTDetails'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='w-[90%] md:w-[95%] mx-auto my-4 sm:my-14'>
+        <Details/>
+        <Category/>
+        <Brand/>
+    </div>
   )
 }
 
