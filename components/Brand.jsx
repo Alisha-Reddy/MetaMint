f06b64635157a93afc4aabce327e295abcc060f6
Alisha-Reddy <98758094+Alisha-Reddy@users.noreply.github.com>
2025-03-12 07:15:14 +0530

    Update Brand.jsx

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -7,7 +7,7 @@ import { Button } from '.'
 const Brand = () => {
   return (
     <div>
-      <div className="my-4 sm:my-14 grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-20 items-center">
+      <div className="my-4 sm:my-14 grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-20 items-center"> 
         <div>
           <Image src={images.logo} alt="brand logo" className='w-1/2 drop-shadow-[5px_5px_4px_black]' />
           <h1 className="text-2xl my-3 font-bold md:text-5xl">Collect, Trade & Earn Crypto with MetaMint!</h1>
